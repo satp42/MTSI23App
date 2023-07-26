5a368ac2ee4f3af1d1a6ff07d171bc176106eb1e
satp42 <msg4satwik@gmail.com>
2023-07-26 10:26:36 -0400

    Push better UI Styling

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,33 +1,46 @@
 'use client';
 
-import { Card, Metric, Text, Title, BarList, Flex, Grid } from '@tremor/react';
+import { Card, Metric, Text, Title, Flex, Grid } from '@tremor/react';
+import Image from 'next/image';
 import Chart from './chart';
-
 
 const data = [
   {
     category: 'Water Intake',
-    stat: '20',
-    // data: app
+    stat: 20,
   }
 ];
 
 export default function PlaygroundPage() {
   return (
     <main className="p-4 md:p-10 mx-auto max-w-7xl">
-      <Grid numItemsSm={2} numItemsLg={3} className="gap-6">
+      <Grid numItemsSm={2} numItemsLg={2} className="gap-6 h-auto grid-auto-rows-fr">
         {data.map((item) => (
-          <Card key={item.category}>
-            <Title>{item.category}</Title>
-            <Flex
-              justifyContent="start"
-              alignItems="baseline"
-              className="space-x-2"
-            >
-              <Metric>{item.stat}</Metric>
-              <Text>cups</Text>
-            </Flex>
-          </Card>
+          <>
+            <Card key={item.category} className="flex flex-col">
+              <Title>{item.category}</Title>
+              <Text>Track how much water you drank today</Text>
+              <Flex
+                justifyContent="start"
+                alignItems="baseline"
+                className="space-x-2"
+              >
+                <Metric>{item.stat}</Metric>
+                <Text>cups</Text>
+              </Flex>
+            </Card>
+            <Card className="flex flex-col">
+              <Title>Your Plant</Title> 
+              <Text>Keep your plant healthy by drinking water</Text>
+              <Flex
+                justifyContent="start"
+                alignItems="baseline"
+                className="space-x-2"
+              >
+                {/* <Image src="/img/3.png" alt="image-for-less-than-23" width={30} height={30} /> */}
+              </Flex>
+            </Card>
+          </>
         ))}
       </Grid>
       <Chart />
